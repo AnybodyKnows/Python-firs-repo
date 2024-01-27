5a3ed57f9040476e47d499512fcc034b32a2fd00
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-01-27 20:48:41 +0100

    first comit

--- 1.py
+++
@@ -1,3 +0,0 @@
-data = { 'year':2024, 'lang':"Pethon", 'version': 3.12}
-cat = {}
-data['lang']
--- main.py
+++
@@ -1,7 +0,0 @@
-message = "Never argue with stupid people, they will dragrrr you down to their level and then beat you with experience."
-search = "r"
-result = 0
-for n in message:
-    if n == search:
-        result +=1
-print (result)
